09edfe1db831a9a99247129e39e7ad7c2eeae257
BIG_whale <f411024@gmail.com>
2015-06-20 23:35:03 +0800

    [FILE I/O]整理一下main
    刪掉重複的部份

--- fatman/main.cpp
+++ fatman/main.cpp
@@ -67,18 +67,6 @@ int main(int argc, char *argv[])
                          for(int a=0;a<group::groups.size();a++){
                             group::groups[a]->print();
                         }
-
-
-    //    //<--funtion use example.this can delete atfer you kown how to use-->
-    //    group g1;
-    //    //creat the object of the group
-    //    g1.add(&r.termarr[1]);
-    //    g1.add(&r.termarr[2]);
-    //    //add shapes into the group
-    //    g1.print();
-    //    //print all of the shape in g1
-    //    cout<<g1.is_in_group(&r.termarr[1])<<endl;
-
                     NOF++; //next file
                 }
                 else{
@@ -97,47 +85,6 @@ int main(int argc, char *argv[])
 
 }//end of file open
 */
-                    ifstream fatmanRead("CADtest.txt", ios::in);
-                    int term_top;
-                    int term_bottom;
-                    int term_left;
-                    int term_right;
-                    int capacity_main=1;
-                    int count=0;
-
-
-                    rectangle r(capacity_main);
-                    while(!fatmanRead.eof())
-                    {
-                        //讀值
-                        fatmanRead>>term_left;
-                        fatmanRead>>term_bottom;
-                        fatmanRead>>term_right;
-                        fatmanRead>>term_top;
-                        r.newterm(term_top,term_bottom,term_left,term_right);
-                        //將讀到的值存進陣列裡
-                        count++;
-                    }
-
-                        r.print();//印出原始數據
-                        r.copy_left_f();//左邊界排序
-                        r.copy_right_f();//右邊界排序
-                        r.copy_top_f();//上邊界排序
-                        r.copy_bottom_f();//下邊界排序
-                        cout<<endl;
-
-                        r.x_grouping();
-                        r.x_group_print();
-                        cout<<endl;
-
-                        r.y_grouping();
-                        r.y_group_print();
-                        cout<<endl;
-
-                         for(int a=0;a<group::groups.size();a++){
-                            group::groups[a]->print();
-                        }
-
     int term_top;
     int term_bottom;
     int term_left;
